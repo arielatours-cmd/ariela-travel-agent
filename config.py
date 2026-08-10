10de1b6743c000d2b6d52a717abe5eac9b924729
arielatours-cmd <arielatours@gmail.com>
2026-08-10 20:20:07 +0300

    9.4

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-APP_VERSION = "9.3.9-persistent-members"
+APP_VERSION = "9.4.0-questionnaire-language-fixes"
 ISRAEL_TZ = "Asia/Jerusalem"
 
 BASE_DIR = Path(__file__).resolve().parent
