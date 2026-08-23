038f1c016d3ce395e1b9f6bfa47b806e69267021
arielatours-cmd <arielatours@gmail.com>
2026-08-23 21:58:38 +0300

    9.7.61

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-APP_VERSION = "9.7.60-testing-start"
+APP_VERSION = "9.7.61-testing-start"
 ISRAEL_TZ = "Asia/Jerusalem"
 
 BASE_DIR = Path(__file__).resolve().parent
