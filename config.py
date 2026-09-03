22428e91464472d42ccc9a5d73f06d08c4644577
arielatours-cmd <arielatours@gmail.com>
2026-09-04 00:30:37 +0300

    Activate Budapest image fix at startup

--- config.py
+++ config.py
@@ -78,3 +78,4 @@ import tools.remove_initial_scan_badge_v136  # noqa: E402,F401
 import tools.customer_rules_cleanup_v136  # noqa: E402,F401
 import tools.whatsapp_9_payment_v136  # noqa: E402,F401
 import tools.final_customer_cleanup_v136  # noqa: E402,F401
+import tools.budapest_image_fix_v136  # noqa: E402,F401
